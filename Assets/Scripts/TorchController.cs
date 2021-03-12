25a5442bf767cfa0985d23a3fdb2fc2f8d4de380
Bubbsy <virgile.aragon@gmail.com>
2021-03-12 11:17:34 +0100

    first commit

---
+++ Assets/Scripts/TorchController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchController : MonoBehaviour
+{
+    public Light ligth;
+    private Transform enemyPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyPosition = GameObject.FindGameObjectWithTag("Enemy").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Mathf.Pow(Vector3.Distance(transform.position, enemyPosition.position), 1.5f);
+        if (distance >= 50)
+            ligth.spotAngle = 50;
+        else
+        {
+            ligth.spotAngle = distance;
+            //ligth.innerSpotAngle = ligth.spotAngle - 9f;
+        }
+            
+
+            
+        
+    }
+}
