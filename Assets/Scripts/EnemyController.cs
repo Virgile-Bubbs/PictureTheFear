72ca9d19e63d15b0114e04b353e51c615baf82c1
Bubbsy <virgile.aragon@gmail.com>
2021-03-12 17:07:42 +0100

    Debut NavMesh

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform[] targets;
+    private NavMeshAgent agent;
+
+    private Transform currentTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.SetDestination(targets[0].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Transform t in targets)
+        {
+            if(transform.position == t.position)
+            {
+                NewDestination();
+            }
+        }
+    }
+
+    void NewDestination()
+    {
+        int indice;
+        do
+        {
+            indice = Random.Range(0, targets.Length - 1);
+        }
+        while (targets[indice] == currentTarget);
+        currentTarget = targets[indice];
+
+        agent.SetDestination(currentTarget.position);
+    }
+}
