fb41232ddcda9f055ec0a6f75b843908a7caa413
Bubbsy <virgile.aragon@gmail.com>
2021-03-14 12:08:24 +0100

    monster nav is ok

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,25 +7,31 @@ public class EnemyController : MonoBehaviour
 {
     public Transform[] targets;
     private NavMeshAgent agent;
+    private Animator animator;
 
     private Transform currentTarget;
 
     // Start is called before the first frame update
     void Start()
     {
-        agent.SetDestination(targets[0].position);
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        animator.SetBool("isWalking", true);
+        NewDestination();
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach(Transform t in targets)
+        Vector3 currentPosition = new Vector3((int)transform.position.x, (int)transform.position.y, (int)transform.position.z);
+        Vector3 targetPosition = new Vector3((int)currentTarget.position.x, (int)currentTarget.position.y, (int)currentTarget.position.z);
+
+        if(currentPosition == targetPosition)
         {
-            if(transform.position == t.position)
-            {
-                NewDestination();
-            }
+            NewDestination();
         }
+
     }
 
     void NewDestination()
@@ -33,7 +39,7 @@ public class EnemyController : MonoBehaviour
         int indice;
         do
         {
-            indice = Random.Range(0, targets.Length - 1);
+            indice = Random.Range(0, targets.Length);
         }
         while (targets[indice] == currentTarget);
         currentTarget = targets[indice];
